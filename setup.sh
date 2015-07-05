9812c71191b682f30fd3f944157465e2d1fa55ea
Carter Thayer <carterwthayer@gmail.com>
2015-07-05 16:24:01 -0500

    Changed to allow linking to a seperate folder on the machine.

--- setup.sh
+++ setup.sh
@@ -1,1 +1,29 @@
-apt-get install libx11-dev xmonad suckless-tools xinit xmobar xterm x11-xserver-utils feh xcompmgr firefox
+#!/bin/bash
+
+### Variables
+DIR=`dirname $0`
+cd $DIR
+DIR=`pwd`
+FILES=".bashrc .vim .wallpaper.png .xmobarrc .xmonad .Xresources .xsessionrc"
+
+### Install programs
+#TODO: Add command line flag to install these
+apt-get install libx11-dev xmonad suckless-tools xinit xmobar xterm x11-xserver-utils feh xcompmgr \
+	firefox python-pip python-dev cmake build-essentials
+pip install virtualenv
+
+### Link in files
+
+for file in $FILES; do
+	if [ -e ~/$file ]
+	then
+		if [ ! -e ~/.dotfiles_old/ ]
+		then
+			mkdir ~/.dotfiles_old/
+		fi
+		mv ~/$file ~/.dotfiles_old/
+	fi
+	ln -s $DIR/$file ~/$file
+done
+
+
