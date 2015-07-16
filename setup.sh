391b6d176fd79c928de26ee67d70e4d1b56c2130
Carter Thayer <carterwthayer@gmail.com>
2015-07-15 19:45:38 -0500

    Added argument options to setup.sh and removed ycm install.

--- setup.sh
+++ setup.sh
@@ -6,8 +6,30 @@ cd $DIR
 DIR=`pwd`
 FILES=".bashrc .vim .wallpaper.png .xmobarrc .xmonad .Xresources .xsessionrc"
 
+PROGRAMS_ARG=false
+FILES_ARG=false
+VIRTUAL_ARG=false
+
+
+for i in "$@"; do
+    if [ $i == "all" ]; then
+	PROGRAMS_ARG=true
+	FILES_ARG=true
+	VIRTUAL_ARG=true
+    fi
+    if [ $i == "programs" ]; then
+	PROGRAMS_ARG=true
+    fi
+    if [ $i == "files" ]; then
+	FILES_ARG=true
+    fi
+    if [ $i == "virtual" ]; then
+	VIRTUAL_ARG=true
+    fi
+done
+
 ### Install programs
-if [ $1=='install' || $2=='install' ]; then
+if [ $PROGRAMS_ARG = true ]; then
     apt-get install libx11-dev xmonad suckless-tools xinit xmobar xterm \
         x11-xserver-utils feh xcompmgr firefox python-pip python-dev cmake \
         build-essentials
@@ -15,27 +37,23 @@ if [ $1=='install' || $2=='install' ]; then
 fi
 
 ### Link in files
-
-for file in $FILES; do
-	if [ -e ~/$file ]
-	then
-		if [ ! -e ~/.dotfiles_old/ ]
-		then
-			mkdir ~/.dotfiles_old/
-		fi
-		mv ~/$file ~/.dotfiles_old/
-	fi
-	ln -s $DIR/$file ~/$file
-done
-
-### Setup YouCompleteMe
-cd .vim/bundle/YouCompleteMe
-git submodule update --init --recursive
-./install.sh --clang-completer
+if [ $FILES_ARG = true ]; then
+    for file in $FILES; do
+    	if [ -e ~/$file ]
+    	then
+    		if [ ! -e ~/.dotfiles_old/ ]
+    		then
+    			mkdir ~/.dotfiles_old/
+    		fi
+    		mv ~/$file ~/.dotfiles_old/
+    	fi
+    	ln -s $DIR/$file ~/$file
+    done
+fi
 
 
 ### Virtual box guest addition
-if [ $1=='virtual' || $2=='virtual' ]; then
+if [ $VIRTUAL_ARG = true ]; then
     mount /dev/cdrom /mnt
     cd /mnt 
     ./VBoxLinuxAdditions.run
