eb7be11d8e400bf6861c5556d4268827e44c3d0a
Carter Thayer <carterwthayer@gmail.com>
2015-07-18 10:34:57 -0500

    Fixed spacing on xmonad files.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -11,15 +11,17 @@ main = do
     xmproc <- spawnPipe "xmobar"
 
     xmonad $ defaultConfig
-        { manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = spacing 3 $ avoidStruts  $  layoutHook defaultConfig
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "#cc6666" "" . shorten 50
-                        }
-        , modMask = mod1Mask
-        , terminal = "xterm"
-	    , borderWidth = 1
-	    , normalBorderColor = "#707880"
-	    , focusedBorderColor = "#cc6666"
+    {
+    manageHook = manageDocks <+> manageHook defaultConfig
+    , layoutHook = spacing 3 $ avoidStruts  $  layoutHook defaultConfig
+    , logHook = dynamicLogWithPP xmobarPP
+        {
+        ppOutput = hPutStrLn xmproc
+        , ppTitle = xmobarColor "#cc6666" "" . shorten 50
         }
+    , modMask = mod1Mask
+    , terminal = "xterm"
+    , borderWidth = 1
+    , normalBorderColor = "#707880"
+    , focusedBorderColor = "#cc6666"
+    }
