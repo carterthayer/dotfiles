4b48121dc323869b8730305672c12e72a17fd488
Carter Thayer <carterwthayer@gmail.com>
2015-07-18 23:01:41 -0500

    Configured xmonad to show urgency on workspace. Change xmobar colors.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,6 +3,7 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Layout.Spacing
 import XMonad.Layout.NoBorders
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.UrgencyHook
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
@@ -10,13 +11,15 @@ import System.IO
 main = do
     xmproc <- spawnPipe "xmobar"
 
-    xmonad $ defaultConfig{
+    xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig{
     manageHook = manageDocks <+> manageHook defaultConfig
     , layoutHook = spacing 3 $ avoidStruts  $  layoutHook defaultConfig
     , logHook = dynamicLogWithPP xmobarPP
         {
         ppOutput = hPutStrLn xmproc
         , ppTitle = xmobarColor "#cc6666" "" . shorten 50
+        , ppCurrent = xmobarColor "#40e2b4" "" . wrap "[" "]"
+        , ppUrgent = xmobarColor "#cc6666" "" . wrap "<" ">"
         }
     , modMask = mod1Mask
     , terminal = "xterm"
