66970b567cd6cbad4cab237b3b8918ebde272e10
Carter Thayer <carterwthayer@gmail.com>
2017-06-19 08:09:47 -0500

    Add some additional installation things, use firefox-esr

--- install.sh
+++ install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+BASEDIR=`dirname $0`
+
 # install requirements from apt-get and pip
 
-sudo apt-get install $(grep -vE "^\s*#" apt_get_installs.txt | tr "\n" " ")
-sudo pip install $(grep -vE "^\s*#" pip_installs.txt | tr "\n" " ")
+sudo apt-get -y update
+
+sudo apt-get -y install $(grep -vE "^\s*#" $BASEDIR/apt_get_installs.txt | tr "\n" " ")
+sudo pip install -U pip
+sudo pip install $(grep -vE "^\s*#" $BASEDIR/pip_installs.txt | tr "\n" " ")
 
 # this is needed for virtualbox guest additions, but requires the uname -r in it
-sudo apt-get install linux-headers-$(uname -r)
+sudo apt-get -y install linux-headers-$(uname -r)
 
 # install sub modules
-pushd requirements/
+pushd $BASEDIR/requirements/
 for i in $(ls -d -- */); do
     pushd $i
     make
@@ -19,10 +24,10 @@ for i in $(ls -d -- */); do
 done
 popd
 
-# link dot files
-pushd home/
-for i in $(ls -A); do
-    rm -r ~/$i || true
-    ln -sv $(pwd)/$i ~/
-done
-popd
+if [[ $(dmesg | grep VirtualBox) ]] && [ -z "$(lsmod | grep vboxguest)" ]; then
+    read -p "Please add VirtualBox guest additions [y/n]:" yn
+        case $yn in
+            [Yy]* ) sudo sh /media/cdrom/VBoxLinuxAdditions.run; break;;
+            * ) break;;
+        esac
+fi
